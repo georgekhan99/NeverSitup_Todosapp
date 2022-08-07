e18fd63b9f60858972502e8894dd87826265149c
Georgekhan <georgekhan99@gmail.com>
2022-08-07 18:24:56 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import LoginForm from '@/components/LoginForm.vue';
 import AllTodos from '@/components/AllTodos.vue';
-/* 1 check token
-   2.if(!token) return login 
-   3.if(token) next
- */
 
 const routes = [
     {
